910779b44a56d92eb93315fd65b77b343610d438
Zain Khan <zainkhanjune@gmail.com>
2022-06-26 20:14:34 +0530

    Added initial files for frontend

---
+++ frontend/captor/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="underline">lets see if it is working</h1>
+      </header>
+    </div>
+  );
+***REMOVED***
+
+export default App;
---
+++ frontend/captor/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config***REMOVED*** */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx***REMOVED***",
+  ***REMOVED***,
+  theme: {
+    extend: {***REMOVED***,
+  ***REMOVED***,
+  plugins: [***REMOVED***,
+***REMOVED***
